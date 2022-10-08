9eda3b3a6ee4d8fe93f9de8bb079f5f0a16e4c6f
motloch <18506389+motloch@users.noreply.github.com>
2022-10-07 22:59:02 -0400

    Polish 04

--- 04_calculate_total_maps.py
+++ 04_calculate_total_maps.py
@@ -1,26 +1,29 @@
 """
-Sum CIB fluxes from individual redshift shells to create the total CIB map
+Create the total CIB maps by summing CIB fluxes from sources located in different redshift
+shells
 """
 import healpy as hp
 import numpy as np
 
+NUM_Z_SHELLS = 23
+FREQUENCIES = ['545', '353', '217']
+
 def get_total_cib_map(which, frequency):
 	"""
 	Add CIB fluxes from individual redshift shells. Allows us to pick which set of maps
-	(unlensed, lensed, ...) we want and which frequency.
+	(unlensed, lensed) we want and which frequency.
 	"""
-	for idx in range(23):
-		if idx == 0:
-			cib_map = hp.read_map('output/' + which + '/smooth_1pix_cib_' + frequency + '_shell_' + str(idx) + '.fits')
-		else:
-			cib_map += hp.read_map('output/' + which + '/smooth_1pix_cib_' + frequency + '_shell_' + str(idx) + '.fits')
+	cib_map = hp.read_map('output/' + which + '/cib_' + frequency + '_shell_0.fits')
+	for idx in range(1,NUM_Z_SHELLS):
+			cib_map += hp.read_map('output/' + which + '/cib_' + frequency + '_shell_' + str(idx) + '.fits')
 
-	hp.write_map(
-			'output/' + which + '/smooth_1pix_cib_' + frequency + '_total.fits', 
-			cib_map,
-			overwrite = True
-		)
+	return cib_map
 
-get_total_cib_map('lensed_proper_magnification', '545')
-get_total_cib_map('lensed_proper_magnification', '353')
-get_total_cib_map('lensed_proper_magnification', '217')
+for frequency in FREQUENCIES:
+	for which in ['lensed', 'unlensed']:
+		cib_map = get_total_cib_map(which, frequency)
+		hp.write_map(
+				'output/' + which + '/cib_' + frequency + '_total.fits',
+				cib_map,
+				overwrite = True
+			)
